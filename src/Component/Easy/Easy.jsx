b8968e935a45f39b39b41759c30b32f8b2423876
Khadija Tul Kubra <49521166+khadijatulkubra1998@users.noreply.github.com>
2019-12-06 02:08:10 +0500

    .

--- src/Component/Easy/Easy.jsx
+++ src/Component/Easy/Easy.jsx
@@ -1,30 +1,44 @@
 import React, { Component } from 'react';
-import { InputNumber } from 'antd';
+import { Button, Icon } from 'antd';
 export class Easy extends Component {
     constructor(props) {
         super(props);
-       
-    this.state = {
-        number : "",
-       
-      };
-    }
-   inputNumber = (event) => {
-        this.setState({ number: event.target.value });
-    }
-    // guess = () => {
-    //     event.preventDefault()
-    //     let answer = parseInt(Math.random() * 100)
-    //     let userAnswer = parseInt({ number })
 
-    // }
+        this.state = { number: "", answer: "",userAnswer:"", };
+    }
+    inputNumber = (e) => {
+        this.setState({ 
+            number: e.target.value,
+            answer : parseInt(Math.random() * 100),
+            userAnswer : parseInt(this.state.number)
+         });
+    }
+
+   
+    
+    guess = () => {
+
+        let guess = this.state.answer === this.state.userAnswer;
+        guess ? alert("yeeeahhh!!! won") : alert("ooopppssss wrong answer")
+       
+    }
     render() {
-        
+        console.log(this.state.number)
+        console.log(this.state.answer)
+
         return (
             <div>
-                <InputNumber
+                <input
+                    min={1}
+                    max={100}
                     defaultValue={3}
                     onChange={this.inputNumber} />
+                <Button type="danger" onClick={() => this.guess()}>Guess</Button>
+                <Button type="primary">
+                    <Icon type="left" />
+                    Backward
+                 </Button>
+
             </div>
         );
     }
